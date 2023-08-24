17a99fa223624020642defb50af983e56faad06f
michellemwangi01 <michellemwangim@gmail.com>
2023-08-24 14:07:56 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 12, "M","white","calm",1),
+("Rowdy", 23, "F","Grey","wild",1),
+("Tabitha", 9, "M","black","violet",1),
+("Sergeant Brown", 8, "M","violet","calm",0),
+("Melissa", 11, "F","purple","feisty",1),
+("Grinch", 12, "F","blue","groggy",1),
+("Wendy", 13, "M","zebra","snookums",1),
+(NULL, 4, "F","white","fluffs",1);
+
